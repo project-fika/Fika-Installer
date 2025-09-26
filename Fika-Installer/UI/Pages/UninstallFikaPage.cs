7afc9e040ae90e336b519ec9a868fc867cb4df64
trippyone <daproblematik@gmail.com>
2025-09-26 08:53:35 -0400

    Reset launcher config when uninstalling

--- Fika-Installer/UI/Pages/UninstallFikaPage.cs
+++ Fika-Installer/UI/Pages/UninstallFikaPage.cs
@@ -1,4 +1,5 @@
 ﻿using Fika_Installer.Spt;
+using System.Text.Json.Nodes;
 
 namespace Fika_Installer.UI.Pages
 {
@@ -20,6 +21,17 @@ namespace Fika_Installer.UI.Pages
             if (!fikaInstaller.UninstallFika())
             {
                 return;
+            }
+
+            JsonObject? launcherConfig = sptInstance.GetLauncherConfig();
+
+            if (launcherConfig != null)
+            {
+                launcherConfig["IsDevMode"] = false;
+                launcherConfig["GamePath"] = installDir;
+                launcherConfig["Server"]["Url"] = "https://127.0.0.1:6969";
+
+                sptInstance.SetLauncherConfig(launcherConfig);
             }
 
             CompositeLogger.Success("Fika uninstalled successfully!", true);
